6b13740a2c9749d1fe982d992de0d2ec00e3fc5c
hushuna <19945096907@136.com>
2019-11-06 10:38:31 +0800

    vue学习demo

---
+++ src/myvue/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="login">
+    <h2>登录</h2>
+    <a href="#">用户名密码</a>
+    <a href="#">短信验证码</a>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Login'
+  }
+</script>
+
+<style scoped>
+  body{
+    background-image: url("/static/img/timg.jpg");
+    background-size: cover;
+  }
+</style>
